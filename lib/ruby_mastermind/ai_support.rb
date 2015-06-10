46c0ad50e666d2917fa07afdb93910695b5a57e5
rickstewart <rick.stewart@ns.sympatico.ca>
2015-06-10 05:38:35 -0300

    June 10

--- lib/ruby_mastermind/ai_support.rb
+++ lib/ruby_mastermind/ai_support.rb
@@ -11,25 +11,57 @@ class AISupport
   end
 
   def test_guess(guess)
-    secret_code = @secret_code
-    @found = []
-    @result = [0, 0]
+    code = @secret_code.clone
+    found = []
+    result = [0, 0]
     # first test catches hits for both correct color and position
     guess.each_with_index do |color, i|
-      if secret_code[i] == color
-        @result[0] += 1
-        @found.push(color)
+      if code[i] == color
+        result[0] += 1
+        found.push(color)
       end
     end
-    # removes any hits ( color and position ) from secret_code list
-    @found.each { |value| secret_code.slice!(secret_code.index(value)) }
+    # removes any hits from test 1
+    found.each { |value| code.slice!(code.index(value)) }
     #second test catches hits for color but wrong position
     guess.each do |color|
-      if secret_code.include? color
-        @result[1] += 1
-        secret_code.slice! secret_code.index(color)
+      if code.include? color.downcase
+        result[1] += 1
+        code.slice! code.index(color.downcase)
       end
     end
-    @result
+    result
+  end
+
+  def validate_input(guess)
+    guess.each do |value|
+      if @COLORS.include? value.downcase
+      else
+        return false
+      end
+    end
+  end
+
+  def get_user_guess(round)
+    puts ""
+    puts "This is round #{round}."
+    puts "For reference the colors are: red, green, blue, yellow, orange, and purple."
+    puts "Now, pick four colors, for example red green yellow red: "
+    gets.split(" ")
+  end
+
+  def welcome
+    puts ""
+    puts "==================================================================================================".bold.green.bg_gray
+    puts "| The game is Mastermind.                                                                        |".green.bg_gray
+    puts "| Your challenge is to guess four randomly generated colors.                                     |".green.bg_gray
+    puts "| The possible colors are: red, green, blue, yellow, orange, and purple.                         |".green.bg_gray
+    puts "| You will have ten rounds to deduce the four colors and their order.                            |".green.bg_gray
+    puts "| At the end of each round you will be told how many of your guesses picked                      |".green.bg_gray
+    puts "| the correct color along with the correct location, and how many correct                        |".green.bg_gray
+    puts "| colors but in the wrong location.                                                              |".green.bg_gray
+    puts "|                                                                                                |".green.bg_gray
+    puts "| Good Luck!                                                                                     |".green.bg_gray
+    puts "==================================================================================================".bold.green.bg_gray
   end
 end
--- lib/ruby_mastermind/main.rb
+++ lib/ruby_mastermind/main.rb
@@ -1,36 +1,27 @@
-# credit to https://stackoverflow.com/questions/1489183/colorized-ruby-output
-# for color code.
 class Main
   require_relative "ai_support"
   require_relative "string"
   require "colorize"
+
   def go
-    print "Please enter your name: "
-    name = gets.chomp
-    puts "==================================================================================================".bold.green.bg_gray
-    puts "| Hello #{name}                                                                                     |".green.bg_gray
-    puts "| The game is Mastermind.                                                                        |".green.bg_gray
-    puts "| #{name}, your challenge is to guess four randomly generated colors.                               |".green.bg_gray
-    puts "| The possible colors are: red, green, blue, yellow, orange, and purple.                         |".green.bg_gray
-    puts "| You will have ten rounds to deduce the four colors and their order.                            |".green.bg_gray
-    puts "| At the end of each round you will be told how many of your guesses picked the correct          |".green.bg_gray
-    puts "| color at the correct location, and how many picked the correct color but in the wrong location.|".green.bg_gray
-    puts "|                                                                                                |".green.bg_gray
-    puts "| Good Luck!                                                                                     |".green.bg_gray
-    puts "==================================================================================================".bold.green.bg_gray
+    valid = false
     ai = AISupport.new
-    ai_code = ai.generate_code
-    puts ai_code.inspect
+    ai.welcome
+    secret_code = ai.generate_code
+    puts secret_code.inspect
     10.times do |round|
       round += 1
-      puts ""
-      puts "This is round #{round}.".bold.brown.bg_blue
-      puts "For reference the colors are: red, green, blue, yellow, orange, and purple."
-      puts "Now, pick four colors, for example red green yellow red: "
-      guess = gets.split(" ")
-      result = ai.test_guess(guess)
-      puts "#{result[0]} hits for correct color and position"
-      puts "#{result[1]} hits for correct color but not position"
+      until valid
+        guess = ai.get_user_guess(round)
+        valid = ai.validate_input(guess)
+        if valid
+          result = ai.test_guess(guess)
+          puts "#{result[0]} hits for correct color and position"
+          puts "#{result[1]} hits for correct color but wrong position"
+        else
+          puts "oops... something was wrong with your guess, try again please."
+        end
+      end
     end
   end
 end
