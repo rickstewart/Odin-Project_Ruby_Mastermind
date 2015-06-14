ec62ffc283e2230e2fda3c0e0bd0437a9c52d399
rickstewart <rick.stewart@ns.sympatico.ca>
2015-06-14 18:27:06 -0300

    June 14

--- lib/ruby_mastermind/main.rb
+++ lib/ruby_mastermind/main.rb
@@ -6,20 +6,53 @@ class Main
     valid = false
     ai = AISupport.new
     ai.welcome
-    ai.generate_code
-    10.times do |round|
-      round += 1
-      until valid
-        guess = ai.get_user_guess(round)
-        valid = ai.validate_input(guess)
-        if valid
-          result = ai.test_guess(guess)
-          ai.display_results(result)
-        else
-          puts "oops... something was wrong with your guess, try again please."
+    role = ai.pick_role
+    if role == "cb"
+      ai.generate_code
+      10.times do |round|
+        round += 1
+        until valid
+          guess = ai.get_user_guess(round)
+          valid = ai.validate_input(guess)
+          if valid
+            result = ai.test_guess(guess)
+            ai.display_results(result)
+          else
+            ai.display_error
+          end
+        end
+        valid = false
+        end_game = ai.win_lose_test(round)
+        if end_game == "win"
+          ai.display_win
+          return
+        elsif end_game == "lose"
+          ai.display_lost
+          return
         end
       end
-      valid = false
+    else
+      until valid
+        code = ai.pick_secret_code
+        valid = ai.validate_input(code)
+        if !valid
+          ai.display_error2
+        end
+      end
+      10.times do |round|
+        round += 1
+        guess = ai.code_break_attempt(round)
+        result = ai.test_guess(guess)
+        ai.display_results(result)
+        end_game = ai.win_lose_test(round)
+        if end_game == "win"
+          ai.display_win
+          return
+        elsif end_game == "lose"
+          ai.display_lost
+          return
+        end
+      end
     end
   end
 end
