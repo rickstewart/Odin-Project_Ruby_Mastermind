bb5069fad531066f6a5912a8ea7aa30b2eb53caa
rickstewart <rick.stewart@ns.sympatico.ca>
2015-06-14 18:26:35 -0300

    June 14

--- lib/ruby_mastermind/ai_support.rb
+++ lib/ruby_mastermind/ai_support.rb
@@ -6,13 +6,54 @@ class AISupport
     @user_guesses = Array.new
   end
 
-  def user_guesses_add(guess, result)
-    @user_guesses << [guess, result]
+  def pick_role
+    while true
+      puts ""
+      puts "What role will you play? For 'CodeBreaker' type cb and for 'CodeMaker' type cm:"
+      role = gets.chomp
+      role.downcase
+      if (role == 'cb') || (role == 'cm')
+        return role
+      else
+        puts "oops... something was wrong with your answer, please pick 'cb' or 'cm'."
+      end
+    end
   end
 
   def generate_code
     4.times { @secret_code.push(@COLORS.sample) }
     @secret_code
+  end
+
+  def pick_secret_code
+    puts ""
+    puts "The colors are: red, green, blue, yellow, orange, and purple."
+    puts "Pick four colors, for example, red green yellow red: "
+    @secret_code = gets.split(" ")
+    @secret_code.map(&:downcase)
+  end
+
+  def get_user_guess(round)
+    puts ""
+    puts "This is round #{round}."
+    puts "The colors are: red, green, blue, yellow, orange, and purple."
+    puts "Pick four colors, for example, red green yellow red: "
+    guess = gets.split(" ")
+    guess.map(&:downcase)
+  end
+
+  def user_guesses_add(guess, result)
+    @user_guesses << [guess, result]
+  end
+
+  def validate_input(guess)
+    guess.each do |value|
+      if (guess.count == 4) && (@COLORS.include? value)
+        return true
+      else
+        return false
+      end
+    end
   end
 
   def test_guess(guess)
@@ -38,12 +79,11 @@ class AISupport
     user_guesses_add(guess, result)
   end
 
-  def validate_input(guess)
-    guess.each do |value|
-      if (guess.count == 4) && (@COLORS.include? value)
-      else
-        return false
-      end
+  def code_break_attempt(round)
+    if round == 0
+      return %w(red red red red)
+    else
+
     end
   end
 
@@ -54,15 +94,6 @@ class AISupport
     elsif round == 10
       "lose"
     end
-  end
-
-  def get_user_guess(round)
-    puts ""
-    puts "This is round #{round}."
-    puts "The colors are: red, green, blue, yellow, orange, and purple."
-    puts "Pick four colors, for example, red green yellow red: "
-    guess = gets.split(" ")
-    guess.map(&:downcase)
   end
 
   def welcome
@@ -89,12 +120,17 @@ class AISupport
     puts "oops... something was wrong with your guess, try again please."
   end
 
+  def display_error2
+    puts "oops... something was wrong with your code, try again please."
+  end
+
   def display_results(result)
     puts "-------------------------------------------- Mastermind --------------------------------------------".colorize(:color => :green, :background => :cyan)
     result.each_with_index do |guess, i|
       puts "%-11s" % "| Round #{i+1} " + "|  " + "%-32s" % "#{guess[0][0]}  #{guess[0][1]}  #{guess[0][2]}  #{guess[0][3]} " + "| #{guess[1][0]} correct color and position | #{guess[1][1]} correct color only"
     end
     puts "----------------------------------------------------------------------------------------------------".colorize(:color => :green, :background => :cyan)
+    puts ""
   end
 
   def display_win
@@ -102,28 +138,6 @@ class AISupport
   end
 
   def display_lost
-    puts "----------------------------------------- Sorry, You Lost! -----------------------------------------".colorize(:color => :white, :background => :black)
-  end
-
-  def pick_role
-    while true
-      puts ""
-      puts "What role will you play? For 'CodeBreaker' type cb and for 'CodeMaker' type cm:"
-      role = gets.split(" ")
-      role.map(&:downcase)
-      if ("role == cb") || ("role == cm")
-        return role
-      else
-        puts "oops... something was wrong with your answer, please pick 'cb' or 'cm'."
-      end
-    end
-  end
-
-  def pick_secret_code
-    puts ""
-    puts "The colors are: red, green, blue, yellow, orange, and purple."
-    puts "Pick four colors, for example, red green yellow red: "
-    secret_code = gets.split(" ")
-    secret_code.map(&:downcase)
+    puts "------------------------------------- Sorry, Computer AI wins! -------------------------------------".colorize(:color => :white, :background => :black)
   end
 end
